801c6cc72907e4ed60dfb7432cb5a899e3743e95
Luccas Oliveira <102065560+luccasfoliveira@users.noreply.github.com>
2023-04-03 15:03:01 -0300

    Update Exec_003.py

--- funcao_string/Exec_003.py
+++ funcao_string/Exec_003.py
@@ -4,6 +4,7 @@ def decimalBase(numero, destino):
         B = "01234567"
     elif destino == 16:
         B = "0123456789ABCDEF"
+
     conversor = ''
     while numero != 0:
         conversor += B[numero % destino]
@@ -19,20 +20,44 @@ def baseDecimal(destino, numero):
         B = "0123456789ABCDEF"
 
     conversor = 0
-    x = 1
-    for i in numero[::-1]:
-        conversor += x * int(i)
-        x *= destino
-    return conversor
+    x = len(numero)-1
+    for i in numero:
+        conversor += destino ** x * B.index(i)
+        x -= 1
+    return int(conversor)
 
 
-base = int(input("Digite a Base: "))
-numero = input("Digite o número: ")
-conversao = int(input("Conversão: "))
+def validacaoBase(base, numero):
+    B = "01"
+    if base == 8:
+        B = "01234567"
+    elif base == 16:
+        B = "0123456789ABCDEF"
+    elif base == 10:
+        B = "0123456789"
 
+    for i in numero:
+        if i not in B:
+            return False
+    return True
+
+print("*" * 30 + " CONVERSÃO DE BASES " + "*" * 30)
+base = int(input("\nDigite a Base de Origem: "))
+while True:
+    numero = input("Digite um número da Base acima: ").upper()
+    if validacaoBase(base, numero):
+        break
+    print("Digite um número válido para a Base....\n")
+
+conversao = int(input("Digite a Base que deseja Converter: "))
 if base != 10 and conversao != 10:
-    print(decimalBase(baseDecimal(base, numero), conversao))
+    resultado = decimalBase(baseDecimal(base, numero), conversao)
 elif base == 10 and conversao != 10:
-    print(decimalBase(int(numero), conversao))
+    resultado = decimalBase(int(numero), conversao)
 else:
-    print(baseDecimal(base, numero))
+    resultado = baseDecimal(base, numero)
+
+print()
+print("*" * 30 + " RESULTADO " + "*" * 30)
+print(f"Número {numero} da base {base} corresponde a {resultado} da base {conversao}")
+print("*" * 30 + " OBRIGADO " + "*" * 30)
